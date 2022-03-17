e2bc804b0d123c3a5548c985ba7c22cd02be2804
BuildTools <unconfigured@null.spigotmc.org>
2022-03-16 21:47:23 -0500

    progress on it

--- Aura's Stuff/OreProcessing.lua
+++ Aura's Stuff/OreProcessing.lua
@@ -14,6 +14,9 @@ local function save()
     local file = io.open("/home/NIDAS/settings/oreFilters", "w")
     file:write(serialization.serialize(oreFilters))
     file:close()
+    local file = io.open("/home/NIDAS/settings/oreAddr", "w")
+    file:write(serialization.serialize(oreAddr))
+    file:close()
     --windowRefresh(searchKey.keyword)          Do I need this? Idk what it does
 end
 
@@ -24,18 +27,49 @@ local function load()
         file:close()
         renderer.update()
     end
+    local file = io.open("/home/NIDAS/settings/oreAddr", "r")
+    if file then
+        oreAddr = serialization.unserialize(file:read("*a")) or {} --TODO get this to work
+        file:close()
+    end
 end
-
+local refresh = nil
+local currentConfigWindow = {}
+--changes config window values
+local function changeAddr(transposerAddress, indexNumber, data)
+    if transposerAddress == "None" then
+        --TODO this
+    else
+        --TODO this
+        oreAddr[indexNumber] = transposerAddress
+    end
+    local x, y, gui, graphics, renderer, page = table.unpack(data)
+    renderer.removeObject(currentConfigWindow)
+    refresh(x, y, gui, graphics, renderer, page)
+end
 
 --provides configuration page from main menu
 function OreProcessing.configure(x, y, gui, graphics, renderer, page)
-    local availabletransposers = {}
+    local renderingData = {x, y, gui, graphics, renderer, page}
+    local number = 1
+    local onActivation = {}
+    graphics.context().gpu.setActiveBuffer(page)
+    graphics.text(3, 5, "Transposer 1:")
+    graphics.text(3, 7, "Transposer 2:")
     for address, componentType in component.list() do
         if componentType == "transposer" then
-            table.insert(availabletransposers, address)
+            table.insert(onActivation, {displayName = displayName, value = changeAddr, args = {address, number, renderingData}})
         end
     end
+    table.insert(onActivation, {displayName = "None", value = changeAddr, args = {"None", renderingData}})
+    table.insert(currentConfigWindow, gui.smallButton(x+15, y+2, oreAddr[1] or "None", gui.selectionBox, {x+16, y+2, onActivation}))
+    local number = 2
+    table.insert(currentConfigWindow, gui.smallButton(x+15, y+3, oreAddr.[2] or "None", gui.selectionBox, {x+16, y+3, onActivation}))
+    local _, ySize = graphics.context().gpu.getBufferSize(page)
     table.insert(currentConfigWindow, gui.bigButton(x+2, y+tonumber(ySize)-4, "Save Configuration", save))
+    
+    renderer.update()
+    return currentConfigWindow
 end
 --provides the button to access filter page
 function OreProcessing.windowButton()\
