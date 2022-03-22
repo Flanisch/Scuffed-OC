cad40f0dd94ea77b73073ad0eaffc07a701056f9
Aurastorm <aurastorm696@gmail.com>
2022-03-21 22:23:07 -0500

    I'm tired of these motherfuckin' graphical bugs on this motherfuckin' plane! And multiAttributeLists are aboslutely physical pain incarnate.

--- Aura's Stuff/OreProcessing.lua
+++ Aura's Stuff/OreProcessing.lua
@@ -7,8 +7,10 @@ local colors = require("lib.graphics.colors")
 local gui = require("lib.graphics.gui")
 local graphics = require("lib.graphics.graphics")
 local renderer = require("lib.graphics.renderer")
+local event = require("event")
 
 --variable initializations
+local dummy = {}
 local transposer = {}
 local editorPage
 drawn = false
@@ -17,13 +19,40 @@ local oreAddr = {}
 local oreFilters = {}
 local search = {}
 local windowRefresh
+local refreshFilters
 local searchKey = {keyword = ""}
+local logoPage
+local pageBuffer = {}
+local input = {}
+local shouldListen = {listen = false, args = {}}
+local filterOutputs = {
+    primary = TBD,
+    orewash = TBD,
+    chembath = TBD,
+    tertiary = TBD,
+    smelt = TBD,
+    sift = TBD,
+    special = TBD
+}
+local outputChoices = {
+    {displayName = "Primary Output", value = filterOutputs.primary},
+    {displayName = "Purify (Ore Wash)", value = filterOutputs.orewash},
+    {displayName = "Purify (Mercury Bath)", value = filterOutputs.chembath},
+    {displayName = "Tertiary Output", value = filterOutputs.tertiary},
+    {displayName = "Smelt", value = filterOutputs.smelt},
+    {displayName = "Sift", value = filterOutputs.sift},
+    {displayName = "Special", value = filterOutputs.special}
+}
 
 local debugnumber = 0
-local function debugnum()
-    print(debugnumber)
+local function debugnum(num)
+    if num ~= nil then
+        print(num)
+    else
+        print(debugnumber)
+        debugnumber = debugnumber + 1
+    end
     require("term").setCursor(1,1)
-    debugnumber = debugnumber + 1
 end
 --- saves table of filters and transposer addresses to file
 local function save()
@@ -57,19 +86,19 @@ end
 local function filterByLabel(data, keyword)
     local filtered = {}
     for key, value in pairs(data) do
-        if string.find(string.lower(value.name), string.lower(keyword)) ~= nil then
+        if string.find(string.lower(key), string.lower(keyword)) ~= nil then
             filtered[key] = value
         end
     end
     return filtered
 end
 
---- searches oreFilters for a name match and returns its index number
+--- searches oreFilters for a name match
 ---@param name string @ filter name
----@return number, boolean
+---@return boolean
 local function searchFilter(name)
     for ID, entry in pairs(oreFilters) do
-        if entry.name == name then
+        if entry == name then
             return ID
         end
     end
@@ -77,31 +106,29 @@ local function searchFilter(name)
 end
 
 --- adds a filter to oreFilters
----@param name string @ desired filter name
----@param filter number @ desired filter output
 ---@return boolean
 local function addFilter(name, filter)
     if not searchFilter(name) then
         table.insert(oreFilters, {name = name, filter = filter})
-        table.sort(oreFilters)
         save()
+        refreshFilters(searchKey.keyword)
         return true
     else
-        return false, "Error: a filter already exists for that"
+        return false
     end
 end
 
 --- modifies a filter to change where that ore goes
----@param filter string @ filter name
----@param desired number @ desired filter output
 ---@return boolean
 local function modifyFilter(filter, desired)
     local target = searchFilter(filter)
     if target then
-        oreFilters.target.filter = desired
+        oreFilters[target].filter = desired
+        save()
+        refreshFilters(searchKey.keyword)
         return true
     else
-        return false, "Error: no filter found"
+        return false
     end
 end
 
@@ -112,9 +139,11 @@ local function removeFilter(name)
     local target = searchFilter(name)
     if target then
         table.remove(oreFilters, target)
+        save()
+        refreshFilters(searchKey.keyword)
         return true
     else
-        return false, "Error: no filter found"
+        return false
     end
 end
 
@@ -128,53 +157,150 @@ local function confirm(x, y)
     local function yes()
         renderer.removeObject(buttonpage)
         context.gpu.fill(x, y, 10, 3, " ")
+        renderer.update()
         return true
     end
     buttonpage = gui.bigButton(x, y, "Confirm?", yes)
+    renderer.update()
 end
 
---- generates a cancel button
----@param page number @ page number to remove
-local function cancelButton(page)
+--- prepares the right side pane for a new page
+local function pagePrep()
     local context = graphics.context()
     local middle = math.floor(context.width / 2)
-    local function cancel(page)
-        renderer.removeObject(page)
-        context.gpu.fill(middle + 1, 1, context.width - middle, context.height - 2, " ")
-        windowRefresh()
+    if #pageBuffer > 0 then
+        renderer.removeObject(pageBuffer)
+        pageBuffer = {}
     end
-    gui.bigButton(context.width - 8, context.height - 5, "Cancel", cancel, page)
+    context.gpu.fill(middle + 1, 1, context.width - middle, context.height - 2, " ")
 end
 
 --- generates a save button at the top right of the screen
----@param name string @ name of the filter
----@param filter number @ output setting of the filter
 ---@param mode string @ either "save" or "modify"
-local function saveButton(name, filter, mode)
+---@param data table @ 
+local function saveButton(mode, data)
     local context = graphics.context()
     local savemode
+    local name = data.name
+    local filter = data.filter
     if mode == "save" then
         savemode = addFilter
     elseif mode == "modify" then
         savemode = modifyFilter
     end
-    gui.bigButton(context.width - 13, 1, "Save Filter", savemode, {name, filter})
+    table.insert(pageBuffer, gui.bigButton(context.width - 13, 1, "Save Filter", savemode, {name, filter}))
+    renderer.update()
+end
+
+local function cancel()
+    pagePrep()
+    windowRefresh()
+    input = {}
+    shouldListen.listen = false
+    shouldListen.args = {}
+    event.ignore("filter_manipulation", saveButton)
+end
+--- generates a cancel button
+local function cancelButton()
+    local context = graphics.context()
+    local middle = math.floor(context.width / 2)
+    table.insert(pageBuffer, gui.bigButton(context.width - 8, context.height - 5, "Cancel", cancel))
+end
+
+--- draws a page's logo
+---@param logo table @ logo information
+---@return number @ NIDAS page number
+local function drawLogo(logo)
+    local context = graphics.context()
+    local middle = math.floor(context.width / 2)
+    renderer.removeObject(logoPage)
+    logoPage = renderer.createObject(middle + 2, 1, #logo[1], 3)
+    context.gpu.setActiveBuffer(logoPage)
+    graphics.outline(1, 1, logo, gui.primaryColor())
+    context.gpu.setActiveBuffer(0)
+    return logoPage
 end
 
 --- draws page to add a filter
 local function addPage()
-    --TODO
+    local context = graphics.context()
+    local middle = math.floor(context.width / 2)
+    local length = context.width - middle - 5
+    local logo = {
+        "◢█◣ ██◣ ██◣   ███ ◥█◤ █   ◥█◤ ███ ██◣",
+        "█▃█ █ █ █ █   █▅   █  █    █  █▄  █ ◤",
+        "◤ ◥ ██◤ ██◤   █   ◢█◣ ███  █  █▆▆ █◥◣"
+    }
+    pagePrep()
+    drawLogo(logo)
+    local editor = renderer.createObject(middle + 2, 5, length, context.height - 7)
+    local nameInput = {}
+    table.insert(pageBuffer, editor)
+    context.gpu.setActiveBuffer(editor)
+    graphics.text(math.floor(length / 2) - 6, 1, "Filter Entry", gui.primaryColor())
+    graphics.text(1, 7, "Filter Name:", colors.white)
+    graphics.text(1, 9, "Filter Output:", colors.white)
+    --TODO rest of data here
+    context.gpu.setActiveBuffer(0)
+    local attributeData = {
+         {name = "", attribute = name, type = "string", defaultValue = "None"},
+         {name = "", attribute = filter, type = "number", defaultValue = "None"}
+    }
+    gui.multiAttributeList(middle + 16, 7, editor, nameInput, attributeData, input)
+    shouldListen.listen = true
+    table.insert(pageBuffer, nameInput)
+    shouldListen.eventID = event.listen("filter_manipulation", saveButton)
+
+    cancelButton()
+    renderer.update()
 end
 
 --- draws page to modify a filter
 ---@param filter string @ name of filter
 local function modifyPage(filter)
-    --TODO
+    local context = graphics.context()
+    local middle = math.floor(context.width / 2)
+    local length = context.width - middle - 5
+    local filter = oreFilters[filter]
+    input[filter] = filter.filter
+    local logo = {
+        "███ ██◣ ◥█◤ ◥█◤   ███ ◥█◤ █   ◥█◤ ███ ██◣",
+        "█▄  █ █  █   █    █▅   █  █    █  █▄  █ ◤",
+        "█▆▆ ██◤ ◢█◣  █    █   ◢█◣ ███  █  █▆▆ █◥◣"
+    }
+    pagePrep()
+    drawLogo(logo)
+    local editor = renderer.createObject(middle + 2, 5, length, context.height - 7)
+    local nameInput = {}
+    table.insert(pageBuffer, editor)
+    context.gpu.setActiveBuffer(editor)
+    graphics.text(math.floor(length / 2) - 6, 1, "Filter Entry", gui.primaryColor())
+    graphics.text(1, 7, "Filter Name:   "..(filter.name or "ERROR"), colors.white)
+    graphics.text(1, 9, "Filter Output:", colors.white)
+    local attributeData = {
+        {name = "", attribute = filter, type = "number", defaultValue = filter.filter or "ERROR"}
+    }
+    gui.multiAttributeList(middle + 16, 8, editor, nameInput, attributeData, input)
+    context.gpu.setActiveBuffer(0)
+
+    cancelButton()
+    renderer.update()
 end
 
+--- draws page showing information
 local function aboutPage()
     local context = graphics.context()
-    gui.bigButton(context.width - 12, 1, "Add Filter", addPage)
+    local middle = math.floor(context.width / 2)
+    local logo = {
+        "◥█◤ █◣█ ███ ◢█◣",
+        " █  ███ █▅  █ █",
+        "◢█◣ █◥█ █   ◥█◤"
+    }
+    pagePrep()
+    drawLogo(logo)
+    --draw other information here
+    table.insert(pageBuffer, gui.bigButton(context.width - 12, 1, "Add Filter", addPage))
+    renderer.update()
 end
 
 --- draws the list of filters, filtered by param filterString
@@ -193,10 +319,10 @@ local function displayFilters(filterString)
         end
         local buttons = {}
         for k, _ in pairs(filters) do
-            label = filters[k]
+            local label = filters[k]
             table.insert(
                 buttons,
-                {name = label.name, func = modifyFilter, args = {k}}
+                {name = label.name, func = modifyPage, args = {k}}
             )
             if k > maxEntries then break end
         end
@@ -207,13 +333,13 @@ local function displayFilters(filterString)
     formCurrentView(filteredFilters)
     renderer.update(idPages)
 end
+refreshFilters = displayFilters
 
-windowRefresh = displayFilters
 --- draws a search box
 local function searchBox()
     local context = graphics.context()
     local middle = math.floor(context.width / 2)
-    local searchFrame = renderer.createObject(1, 1, middle - 2, 3)
+    local searchFrame = renderer.createObject(1, 1, middle - 1, 3)
     local top = "╭"
     local mid = "│"
     local bottom = "╰"
@@ -236,7 +362,7 @@ end
 --[[
 oreFilters is a table with the following format:
 oreFilters {
-    1   {name = [name of ore], filter = [1-7]}
+    {name="[name of ore]",filter=[number]}
 }
 the filters are:
 1: primary output (macerate-centrifuge-output)
@@ -256,6 +382,7 @@ local function returnToMenu()
     renderer.switchWindow("main")
     renderer.clearWindow("OreProcessing")
     renderer.update()
+    debugnumber = 0 --debug line
 end
 
 ---main window draw
@@ -275,7 +402,6 @@ local function displayWindow()
     local bar = bar .. "▂▂▂"
     graphics.text(1, 1, bar, gui.borderColor())
     graphics.text(middle, 1, "▟", gui.borderColor())
-    --context.gpu.setActiveBuffer(0)
     --draw middle divider
     local dividervert = renderer.createObject(middle, 1, 1, height - 2)
     context.gpu.setActiveBuffer(dividervert)
@@ -287,12 +413,10 @@ local function displayWindow()
     displayFilters()
     searchBox()
     aboutPage()
-
     renderer.update()
-    --for k, v in pairs(oreFilters) do for i, j in pairs(v) do print(i, j) end end
 end
 
-
+windowRefresh = aboutPage
 local refresh
 local currentConfigWindow = {}
 ---changes config window values
@@ -359,6 +483,17 @@ function OreProcessing.update()
     if drawn and lastKeyword ~= searchKey.keyword then
         displayFilters(searchKey.keyword)
         lastKeyword = searchKey.keyword
+    end
+    if shouldListen.listen then
+        debugnum()
+        if input.name and input.filter then 
+            if string.match(tostring(input.filter), "[1234567]") then
+                shouldListen.args = input
+                event.push("filter_manipulation", shouldListen.args)
+                shouldListen.listen = false
+                shouldListen.args = {}
+            end
+        end
     end
 end
 
